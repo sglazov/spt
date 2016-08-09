b8caa201317d31cf923b8902f58abbf56f6a2673
4enki <mail@4enki.ru>
2016-08-09 12:50:52 +0300

    see #3: v1.3.5; new version on postcss-extend; fix font-family on var-file.sss; fix index (_start.html) file; create progress.html & guideline.html files; small add in JS; fix readme file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // подсвечиваем ссылки с одинаковым адресом
   $(document).on('mouseover mouseout', "a", function(e) {
   	var href = $(this).attr('href');
 			if (!href || href == '#') {
@@ -8,6 +9,18 @@ $(document).ready(function() {
     $("a")
       .filter('[href="' + $(this).attr('href') + '"]')
       .toggleClass("hover", e.type == 'mouseover');
+  });
+
+  // спойлер-блок на странице
+  $('.foldable--content').hide()
+    $('.foldable').click(function(){
+      $(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
+      if($(this).hasClass('folded')) {
+        $('.foldable-link').html('Свернуть программу семинара');
+      }
+      else {
+        $('.foldable-link').html('Показать программу семинара');
+      }
   });
 
 });
