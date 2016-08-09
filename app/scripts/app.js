eee236125372abd35d2aabf116623f0354433818
4enki <mail@4enki.ru>
2016-08-09 14:39:28 +0300

    see #3: create guideline.html content; fix _start.html styles; add JS for footnote; adding styles elemets on type.sss

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,15 +12,111 @@ $(document).ready(function() {
   });
 
   // спойлер-блок на странице
-  $('.foldable--content').hide()
-    $('.foldable').click(function(){
+  $('.foldable__content').hide()
+    $('.foldable__control').click(function(){
       $(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
       if($(this).hasClass('folded')) {
-        $('.foldable-link').html('Свернуть программу семинара');
+        $('.foldable__link').html('Свернуть раскрывающийся контент');
       }
       else {
-        $('.foldable-link').html('Показать программу семинара');
+        $('.foldable__link').html('Показать раскрывающийся контент');
       }
   });
 
 });
+
+
+$(function(){
+ main.footnote.init();
+});
+var main = {};
+
+main.footnote = {
+ init: function(){
+  var t = this,
+   footnote = $('.footnote'),
+   is_footnotes,
+   part = 0,
+   markers = [],
+   contents = [];
+
+  footnote.each(function(){
+   var marker = $(this), footnotes = marker.closest('.footnotes').addClass('footnotes_inited');
+   if(footnotes.length){
+    if(!is_footnotes){
+     contents[part] = {};
+     is_footnotes = true;
+    }
+    contents[part][marker.text()] = $('<div class="footnote_hint" style="position: absolute; z-index: 9999;"></div>').
+     append(
+      marker.
+      parent().
+      nextUntil('*:has(.footnote)').
+      andSelf().
+      clone()
+     ).
+     hide().
+     appendTo('body').
+     click(function(){return false;});
+   }else{
+    if(is_footnotes){
+     part++;
+     is_footnotes = false;
+    }
+    markers.push({element: marker, part: part});
+   }
+  });
+
+  $(document).click(function(){
+   if(t.opened){
+    t.opened.close();
+   }
+  });
+
+  for(var i = markers.length - 1, marker; i >= 0 ; i--){
+   var marker = markers[i], id = marker.element.text();
+   if(contents[marker.part] && contents[marker.part][id]){
+    t.items.push(new t.item(marker.element, contents[marker.part][id]));
+   }
+  }
+ },
+
+ items: [],
+
+ item: function(control, content){
+  var t = this;
+  t.control = control.css('cursor', 'pointer').addClass('footnote_inited');
+  t.content = content;
+  control.click(function(){
+   if(t.opened){
+    t.close();
+   }else{
+    t.open();
+   }
+   return false;
+  });
+ }
+};
+
+main.footnote.item.prototype = {
+ open: function(){
+  if(main.footnote.opened){
+   main.footnote.opened.close();
+  }
+  this.opened = true;
+  var offset = this.control.offset(), body_width = $('body').width();
+  if(offset.left < body_width / 2){
+   this.content.css({top: offset.top + this.control.outerHeight(), left: offset.left + this.control.outerWidth(), right: 'auto'});
+  }else{
+   this.content.css({top: offset.top + this.control.outerHeight(), right: body_width - offset.left, left: 'auto'});
+  }
+  this.content.show();
+  main.footnote.opened = this;
+ },
+
+ close: function(){
+  this.opened = false;
+  this.content.hide();
+  main.footnote.opened = null;
+ }
+};
